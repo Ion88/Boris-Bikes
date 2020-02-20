8659cdddbc651df51827d4d3edff3da67bec2e7f
Ion <ion.vizitiu88@gmail.com>
2020-02-20 20:23:19 +0000

    chapter 17

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,20 +1,34 @@
 #require_relative 'bike'
 
 class DockingStation
-  #attr_reader = :bike
+  DEFAULT_CAPACITY = 20
 
-  def initialize
+  attr_accessor :capacity
+
+  def initialize(capacity)
     @bikes = []
+    @capacity = capacity
   end
 
   def release_bike
-    fail 'No bikes available' if @bikes.empty?
+    fail 'No bikes available' if empty?
     @bikes.pop
   end
 
   def dock(bike)
-    fail 'Docking station full' if @bikes.count >= 20
+    fail 'Docking station full' if full?
     @bikes << bike
   end
 
+private
+
+attr_reader :bikes
+
+def full?
+  @bikes.count >= capacity
+end
+
+ def empty?
+   @bikes.empty?
+ end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,12 +1,16 @@
 require 'docking_station'
 
 describe DockingStation do
-  it { is_expected.to respond_to :release_bike }
+  #it { is_expected.to respond_to :release_bike }
+  it 'respond to release_bike' do
+    expect(subject).respond_to? :release_bike
+  end
 
   describe '#release_bike' do
     it 'raises an error when there are no bikes available' do
       expect { subject.release_bike}.to raise_error 'No bikes available'
     end
+    #Arrange
     it 'releases a bike' do
       bikes = Bike.new
       subject.dock(bikes)
@@ -17,21 +21,29 @@ describe DockingStation do
   it { is_expected.to respond_to(:dock).with(1).argument }
   #it { is_expected.to respond_to(:bike) }
 
+#Arrange
   it 'docks something' do
     bike = Bike.new
+    #Act/Assert
     expect(subject.dock(bike)).to eq [bike]
   end
 
-  # it 'returns docked bikes' do
-    # bike = Bike.new
-    # subject.dock(bike)
-    # expect(subject.bike).to eq bike
-  # end
+  #it 'returns docked bikes' do
+  #  bike = Bike.new
+  #  subject.dock(bike)
+  #  expect(subject.bike).to eq bike
+  #end
 
   describe '#dock' do
     it 'raises an error when full' do
-      20.times { subject.dock Bike.new }
-      expect { subject.dock Bike.new}.to raise_error 'Docking station full'
+      subject.capacity.times { subject.dock Bike.new }
+      expect { subject.dock Bike.new }.to raise_error 'Docking station full'
+    end
+  end
+
+  describe DockingStation do
+    it 'has a default capacity' do
+      expect(subject.capacity).to eq DockingStation::DEFAULT_CAPACITY
     end
   end
 end
